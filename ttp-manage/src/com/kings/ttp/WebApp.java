fa19128d7d83fb63610ef7124e99690124210e35
kingsfighter <wangwu5253232@gmail.com>
2013-12-14 19:37:53 +0800

    初始化包结构

--- ttp-manage/src/com/kings/ttp/WebApp.java
+++ ttp-manage/src/com/kings/ttp/WebApp.java
@@ -1,5 +1,12 @@
 package com.kings.ttp;
 
+import org.springframework.context.ApplicationContext;
+/**
+ * Web基础控制类
+ * @author Kfighter
+ *
+ */
 public class WebApp {
-
+	//Spring容器
+	public static ApplicationContext context;
 }
---
+++ ttp-manage/src/com/kings/ttp/web/init/SystemInit.java
@@ -0,0 +1,15 @@
+package com.kings.ttp.web.init;
+
+import com.kings.ttp.WebApp;
+
+/**
+ * 系统初始化类
+ * 
+ * @author Kfighter
+ * 
+ */
+public class SystemInit {
+	public void init() {
+		WebApp.context = null;
+	}
+}
